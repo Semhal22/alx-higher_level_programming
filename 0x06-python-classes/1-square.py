a6b0b09a27b3cc144c0bf89a66dbf75f8d99f5c0
Semhal Desta <semhalgidena@gmail.com>
2023-05-30 18:18:31 -0700

    Instantiate a private attribute in class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A class with a private instance attribute"""
+    def __init__(self, size=0):
+        """
+        Instantiate size attribute
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,4 +3,5 @@
 <p>This directory includes tasks related to classes and objects in python.</p>
 <ul>
 <li>0. My first square</li>
+<li>1. Square with size<li>
 </ul>
