0647e2b570c090c546835a9138b1d79ff659f301
Semhal Desta <semhalgidena@gmail.com>
2023-06-12 20:18:39 -0700

    Comment modules

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Module that has one function lookup"""
+
 
 def lookup(obj):
     """Lookup avaliable methods and atributes of an object
+
     Args:
         obj: The object
-    Return:
+
+    Returns:
         list: of the attributes and methods
     """
     return dir(obj)
