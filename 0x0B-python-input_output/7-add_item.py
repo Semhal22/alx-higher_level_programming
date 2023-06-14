5d00c6d0bc2cc26a34c0c0ed6a8942f78320b93e
Semhal Desta <semhalgidena@gmail.com>
2023-06-13 18:58:12 -0700

    Loads from file if it exists

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module that imports save_to_json_file, load_from_json_file"""
 
+
 import sys
+import os
 """Adds all arguments to a Python list and then save them to a file
 Pushes an empty list to the file if it doesn't exit
 """
@@ -9,9 +11,8 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arguments = sys.argv[1:]
 filename = "add_item.json"
-try:
+content = []
+if os.path.exists(filename):
     content = load_from_json_file(filename)
-    content += arguments
-except FileNotFoundError:
-    content = []
+content += arguments
 save_to_json_file(content, filename)
