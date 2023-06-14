b0f4164392a21d74003ae72e47105dde947c694b
Semhal Desta <semhalgidena@gmail.com>
2023-06-14 14:09:28 -0700

    Create a Base Class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with a method area"""
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
