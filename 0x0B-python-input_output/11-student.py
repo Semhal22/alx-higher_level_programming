4c43e7aee2e27f83635a44374c3c759fecabc52e
Semhal Desta <semhalgidena@gmail.com>
2023-06-13 18:47:58 -0700

    Use get() to retrieve value from dictionary

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -36,6 +36,6 @@ class Student:
         Args:
             dict: json
         """
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        self.first_name = json.get("first_name", self.first_name)
+        self.last_name = json.get("last_name", self.last_name)
+        self.age = json.get("age", self.age)
