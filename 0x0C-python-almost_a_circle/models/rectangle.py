3972c1f5ff5b061891cb2a2fe885f9a36d24c34c
Semhal22 <semhalgidena@gmail.com>
2023-06-19 13:47:15 -0700

    Update display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,7 +109,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print Rectangle with character #"""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
+            for _ in range(self.x):
+                print(" ", end="")
             for _ in range(self.width):
                 print("#", end="")
             print()
