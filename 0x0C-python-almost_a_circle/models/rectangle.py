363c2a47ed35d4d667d7611db45709292a9fb4b8
Semhal22 <semhalgidena@gmail.com>
2023-06-19 13:08:45 -0700

    Typing Error(change width with height)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -57,7 +57,7 @@ class Rectangle(Base):
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
