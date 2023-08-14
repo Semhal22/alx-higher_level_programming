15150d446ec744f6d7941a9735a54ff2f9d4c2ae
Semhal22 <semhalgidena@gmail.com>
2023-08-14 23:01:30 +0000

    Convert strings to number using unary plus

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,22 +7,22 @@ if (lenArr === 0 || lenArr === 1) {
   console.log(0);
 } else {
   let max = findMax(input, lenArr);
-  let newArr = argv;
+  let newArr = input;
   // const index = newArr.indexOf(max);
 
   // newArr.splice(index, 1);
   newArr = input.filter((arg) => {
-    return arg !== max;
+    return +arg !== max;
   });
   max = findMax(newArr, newArr.length);
   console.log(max);
 }
 
 function findMax (arr, len) {
-  let max = arr[0];
+  let max = +arr[0];
   for (let i = 1; i < len; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+    if (+arr[i] > max) {
+      max = +arr[i];
     }
   }
   return max;
