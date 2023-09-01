61c9b0e8ca36c392dbcfeb3d951b0e03415a0515
Semhal22 <semhalgidena@gmail.com>
2023-09-01 22:04:57 +0000

    Get value of a variable in the header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Fetches a url and displays value of a variable in the response header"""
+import urllib.request
+import sys
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    value = response.getheader('X-Request-Id')
+    print(value)
