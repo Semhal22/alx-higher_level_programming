eec8a7556dc58740b578b4b351457138b3a3ef40
Semhal22 <semhalgidena@gmail.com>
2023-09-01 22:47:17 +0000

    Use text attribute of body

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import requests
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
-    body = response.content
+    body = response.text
     print('Body response:')
     print(f'\t- type: {type(body)}')
-    print(f'\t- content: {body.decode("utf-8")}')
+    print(f'\t- content: {body}')
