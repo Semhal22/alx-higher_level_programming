0bba71f71729bbac6169815638756a0b53acbcf0
Semhal22 <semhalgidena@gmail.com>
2023-09-01 23:05:27 +0000

    Handles error using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays body of the response"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: ", response.status_code)
+    else:
+        print(response.text)
