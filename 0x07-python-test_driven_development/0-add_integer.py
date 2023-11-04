aa14901b0f21d7f25740ccb31148be0948a549d4
Semhal22 <semhalgidena@gmail.com>
2023-11-04 00:00:02 +0000

    Test for a simple function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/ur/bin/python3
+""" Contains a function add_integer """
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers(casted to integers if floats)
+    Exceptions:
+    TypeError: if a and b are not integers or floats
+    Return: an integer
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+# 0x07. Python - Test-driven development
+## It is always write documentation and tests first before actually writing the code. Includes doctest and unitest
+* 0. Integers addition
