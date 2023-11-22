5cd6aa62d951782bf4dfa6e4d01962325f0435c4
Semhal22 <semhalgidena@gmail.com>
2023-11-22 23:35:05 +0000

    Add function with doctest

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Contains a function say_my_name """
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is first_name last_name"
+    Exceptions:
+        TypeError - if first_name and last_name are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
