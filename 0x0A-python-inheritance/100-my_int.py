8413dbde74a5e5e86b368eaa82e44653462ea2f1
Semhal22 <semhalgidena@gmail.com>
2023-11-27 14:52:15 +0000

    Operator overloading of type int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+"""Inherits from int and modifies some methods"""
+
+
+class MyInt(int):
+    """Inherits from int and operators == and != are inverted"""
+    def __init__(self, a):
+        self.a = a
+
+    def __eq__(self, other):
+        """Redefine '==' to mean '!=' """
+        if self.a != other:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        """Redefine '!=' to mean '==' """
+        if self.a == other:
+            return True
+        else:
+            return False
