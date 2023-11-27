f88f9049278385f55a6c642a3eead3176472874b
Semhal22 <semhalgidena@gmail.com>
2023-11-27 15:13:20 +0000

    Function that adds new attribute to an object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Adds a new attribute to an object if it's possible
+or raises a TypeError if not"""
+
+
+def add_attribute(obj, attr, value):
+    if type(obj) in [int, float, list, dict, str, bool]:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr, value)
