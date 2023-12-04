25af3165dc69bb1ed77023a4343cbcc5b39abd64
Semhal22 <semhalgidena@gmail.com>
2023-12-04 15:59:47 +0000

    Add tests for the base class

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,5 +1,6 @@
-<h2>0x0C. Python - Almost a circle</h2>
-<p>This directory contains tasks covering topics so far about Python like Import, Exceptions, Class etc.... Also new concept args and kwargs is introduced</p>
-<ul>
-<li>1. Base class</li>
-</ul>
+# 0x0C. Python - Almost a circle
+
+## This directory contains tasks covering topics so far about Python like Import, Exceptions, Class etc.... Also new concept args and kwargs is introduced</p>
+
+### 1. Base class
+- Write the first class Base. This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,26 +1,35 @@
-import unittest
-import sys
-path_ = '/root/alx-higher_level_programming/0x0C-python-almost_a_circle'
-sys.path.insert(0, path_)
+#!/usr/bin/python3
+
+import models
 from models.base import Base
 from models import base
+import unittest
 
 
 class TestBase(unittest.TestCase):
-    def test_documentation(self):
-      self.assertGreater(len(base.__doc__), 1)
-      self.assertGreater(len(Base.__doc__), 1)
-      self.assertGreater(len(Base.__init__.__doc__), 1)
-    def setUp(self):
-        self.b1 = Base()
-        self.b2 = Base(4)
-        self.b3 = Base(0)
-        self.b4 = Base(-2)
+    """Tests the base class"""
+    def test_module_documentation(self):
+        """Documentation for the module"""
+        self.assertGreater(len(base.__doc__), 1)
+
+    def test_class_documentation(self):
+        """Documentation of the class Base"""
+        self.assertGreater(len(Base.__doc__), 1)
+
+    def test_func_documentation(self):
+        """Documentation of the function init"""
+        self.assertGreater(len(Base.__init__.__doc__), 1)
+
     def test_values(self):
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 4)
-        self.assertEqual(self.b3.id, 3)
-        self.assertEqual(self.b4.id, 5)
-    @classmethod
-    def setUpClass(cls):
-        Base._Base__nb_objects = 0
+        """Check if correct id's are set to instances of the Base class"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
