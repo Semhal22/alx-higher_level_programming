16f82e53fcddec5683090e0203bb26b8d55f1a2e
Semhal22 <semhalgidena@gmail.com>
2023-12-05 16:41:30 +0000

    Test for method to_dict of Rectangle class

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -110,3 +110,20 @@ class TestBase(unittest.TestCase):
 
         r1.update(x=1, height=2, y=3, width=4, id=23)
         self.assertEqual(str(r1), "[Rectangle] (23) 1/3 - 4/2")
+
+    def test_to_dictionary(self):
+        """Tests the method to_dictionary of the Rectangle class"""
+        r1 = Rectangle(10, 2, 1, 9)
+        self.assertEqual(str(r1), "[Rectangle] (1) 1/9 - 10/2")
+
+        r1_dict = r1.to_dictionary()
+        self.assertEqual(r1_dict, {'x': 1, 'y': 9, 'id': 1, 'height': 2,
+                                   'width': 10})
+        self.assertIsInstance(r1_dict, dict)
+
+        r2 = Rectangle(1, 1)
+        self.assertEqual(str(r2), "[Rectangle] (2) 0/0 - 1/1")
+
+        r2.update(**r1_dict)
+        self.assertEqual(str(r2), "[Rectangle] (1) 1/9 - 10/2")
+        self.assertNotEqual(r1, r2)
