9ea445b907c85308d995ffba801475114a3bc0ad
Semhal22 <semhalgidena@gmail.com>
2023-12-12 23:46:20 +0000

    Order by DESC

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Computes the temp average of all records in the table temperatures
 -- Result is city and the avg_temp
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
