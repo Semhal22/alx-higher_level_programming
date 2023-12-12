de9d634fb067379fa727426f5c347ab6a177fc2c
Semhal22 <semhalgidena@gmail.com>
2023-12-12 22:29:00 +0000

    Used ROW to add new rows to table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,3 @@
 -- creates a table second_table in the database hbtn_0c and add records
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES(1, "Jhon", 10);
-INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES(4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES ROW(1, "Jhon", 10), ROW(2, "Alex", 3), ROW(3, "Bob", 14), ROW(4, "George", 8);
