8387440c125816e42dcff911b6f5f1bfdcec8012
Semhal22 <semhalgidena@gmail.com>
2023-12-13 14:33:45 +0000

    Corrected typo error on database name

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates database hbtn_0d_02 and user user_0d_2
 -- User should have only SELECT privilege on database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_02.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
