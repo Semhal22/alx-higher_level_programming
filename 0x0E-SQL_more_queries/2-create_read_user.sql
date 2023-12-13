eccca6f6cc4a147e44226a9c30f9f931f9b4a0fb
Semhal22 <semhalgidena@gmail.com>
2023-12-13 14:31:25 +0000

    Grant specific privilege to user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates database hbtn_0d_02 and user user_0d_2
+-- User should have only SELECT privilege on database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_02.* TO 'user_0d_2'@'localhost';
