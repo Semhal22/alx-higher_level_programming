707be01922e9c358591601ae1d2b2c0717490c3b
Semhal22 <semhalgidena@gmail.com>
2023-12-13 15:39:31 +0000

    Make Keywords in uppercase

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name from cities where state_id = (
-	SELECT id from states where name = 'California'
+SELECT id, name from cities WHERE state_id = (
+	SELECT id FROM states WHERE name = 'California'
 ) ORDER BY id;
