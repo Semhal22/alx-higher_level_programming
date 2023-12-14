cb2171ce90f47430047caa0127af44ea01413482
Semhal22 <semhalgidena@gmail.com>
2023-12-14 01:39:33 +0000

    List genres not related to a specific show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- list all genres not linked to the show Dexter
+SELECT DISTINCT tv_genres.name AS name
+  FROM tv_genres LEFT JOIN tv_show_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  AND tv_show_genres.show_id = (
+	SELECT id FROM tv_shows
+	WHERE title = 'Dexter'
+  )
+  WHERE tv_show_genres.show_id IS NULL
+  ORDER BY tv_genres.name;
