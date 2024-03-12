f7555d8620668ff8958e90b8da16056bb727b56c
Semhal22 <semhalgidena@gmail.com>
2024-03-12 00:53:13 +0000

    Update token

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const obj = require('./100-data');
-const list = obj.list;
-const newList = list.map((value, index) => value * index);
-console.log(list);
-console.log(newList);
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,4 +0,0 @@
-# 0x13. JavaScript - Objects, Scopes and Closures
-## Using objects and methods
-1. Rectangle #0
-
