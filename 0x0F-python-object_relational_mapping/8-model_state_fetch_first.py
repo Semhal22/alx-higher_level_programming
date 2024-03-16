f445f0b04bccecd43e37e62415b192875c96e895
Semhal22 <semhalgidena@gmail.com>
2024-03-16 17:23:12 +0000

    Retrieve the first State Object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints the first State Object from database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user, pwrd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    path = f'mysql+mysqldb://{user}:{pwrd}@localhost/{db}'
+    engine = create_engine(path, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.id).first()
+    if not result:
+        print()
+    else:
+        print(f"{result.id}: {result.name}")
