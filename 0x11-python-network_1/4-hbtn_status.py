913f412019d08ea8747a24daa1f5d2667bf5217f
Semhal Gidena <semhalgidena@gmail.com>
2024-03-30 00:00:49 +0000

    Used tab for output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"    - type: {type(res.text)}")
-    print(f"    - content: {res.text}")
+    print(f"	- type: {type(res.text)}")
+    print(f"	- content: {res.text}")
