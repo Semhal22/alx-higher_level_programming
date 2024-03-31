a9a50f70994ca359d5fdf423d2a45e883e43666d
Semhal Gidena <semhalgidena@gmail.com>
2024-03-31 20:27:10 +0000

    Add /users in url

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     username, passwrd = sys.argv[1], sys.argv[2]
     headers = {'Authorization': f'Bearer {passwrd}',
                'Accept': 'application/vnd.github+json'}
-    res = requests.get(f"https://api.github.com/{username}", headers=headers)
+    url = f"https://api.github.com/users/{username}"
+    res = requests.get(url, headers=headers)
     user = res.json()
-    if (hasattr(user, 'id')):
-        print("user['id']")
-    else:
-        print("None")
+    print(user.get('id'))
