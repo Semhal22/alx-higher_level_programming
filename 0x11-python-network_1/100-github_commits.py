0b0dc1eb9f1cc0d65fc8090b1d21c4156eaf04a2
Semhal Gidena <semhalgidena@gmail.com>
2024-03-31 20:54:21 +0000

    Handle error

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     headers = {'Accept': 'application/vnd.github+json'}
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     res = requests.get(url, headers=headers)
-    res = res.json()
-
-    for i in range(10):
-        obj = res[i]
-        tx = f"{obj.get('sha')}: {obj.get('commit').get('author').get('name')}"
-        print(tx)
+    if (res.status_code == 200):
+        res = res.json()
+        for i in range(10):
+            obj = res[i]
+            author = obj.get('commit').get('author').get('name')
+            tx = f"{obj.get('sha')}: {author}"
+            print(tx)
